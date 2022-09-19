3f2a040e851ad559cb6843ce350aca9fb78c45b4
Tom_G <tamrico2697@gmail.com>
2022-09-19 19:29:09 +0300

    Add js

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,7 @@ const nojsElement = document.querySelector('.nojs');
 nojsElement.classList.remove('nojs');
 
 const buttonToggle = document.querySelector('.toggle');
+const buttonToggleClose = document.querySelector('.toggle--close');
 const navSelector = document.querySelector('.navigation__list');
 
 //buttonToggle.classList.remove('navigation__list--closed');
@@ -11,6 +12,13 @@ console.dir('buttonToggle');
 const openMenu = function(){
   navSelector.classList.toggle('navigation__list--closed');
   navSelector.classList.toggle('navigation__list--open');
+  console.log('1');
+}
+const closeMenu = function(){
+  navSelector.classList.toggle('navigation__list--closed');
+  navSelector.classList.toggle('navigation__list--open');
+  console.log('2');
 }
 
 buttonToggle.addEventListener('click', openMenu);
+buttonToggleClose.addEventListener('click', closeMenu);
